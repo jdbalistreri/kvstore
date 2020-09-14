eeeb1e9b73d1c423cf5a046d97e99054fc705a2b
Joe Balistreri <jbalistreri@twitter.com>
2020-09-14 13:24:14 -0700

    implement encoding

--- server.py
+++ server.py
@@ -1,30 +1,32 @@
 import socketserver
 
 from kvstore.constants import GET_OP, SET_OP
-from kvstore.input import parse_input, InputValidationError
+from kvstore.input import input_to_command, InputValidationError
 from kvstore.store import KVStore
+from kvstore.encoding import *
 
 
 class EchoRequestHandler(socketserver.BaseRequestHandler):
     def setup(self):
         self.kvstore = KVStore()
+        self.en = BinaryEncoderDecoder()
 
-    def getResult(self, i):
+    def getResult(self, data):
         try:
-            op, key, value = parse_input(i)
+            command = self.en.decode(data)
         except InputValidationError as e:
             return str(e)
 
-        if op == GET_OP:
-            result = self.kvstore.get(key)
-        elif op == SET_OP:
-            result = self.kvstore.set(key, value)
+        if command.enum == CommandEnum.GET:
+            result = self.kvstore.get(command.key)
+        elif command.enum == CommandEnum.SET:
+            result = self.kvstore.set(command.key, command.value)
 
         return result
 
     def handle(self):
         data = self.request.recv(1024)
-        result = self.getResult(data.decode(encoding='UTF-8'))
+        result = self.getResult(data)
         self.request.send(result.encode(encoding='UTF-8'))
         return
 
